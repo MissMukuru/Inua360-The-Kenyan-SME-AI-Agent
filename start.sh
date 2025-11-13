23d9096936aa4eb1af52d3b23dff455cbdfea46b
MissMukuru <a.mukuruzawadi@gmail.com>
2025-11-13 17:46:50 +0300

    Made cahned to teh rendeer file

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uvicorn inua360_the_kenyan_sme_ai_agent.predictions_api:app --host 0.0.0.0 --port $PORT
+uvicorn inua360_the_kenyan_sme_ai_agent.prediction.predictions_api:app --host 0.0.0.0 --port ${PORT:-8000}
