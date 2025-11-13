0a21b5ba61273d538691f91eb33d9d3a8b13025a
MissMukuru <a.mukuruzawadi@gmail.com>
2025-11-13 16:59:55 +0300

    Added shortened prod url

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd inua360_the_kenyan_sme_ai_agent
-uvicorn inua360_the_kenyan_sme_ai_agent.modeling.predictions_api:app --host 0.0.0.0 --port $PORT
+uvicorn inua360_the_kenyan_sme_ai_agent.predictions_api:app --host 0.0.0.0 --port $PORT
